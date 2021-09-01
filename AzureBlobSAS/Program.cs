4deb81d68f1fb61dc7fb5915b97a40515d10af21
Lin <lin.special@gmail.com>
2021-09-01 09:08:57 -0400

    Add project files.

---
+++ AzureBlobSAS/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureBlobSAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task dl = dl_task();
+            dl.Wait();
+        }
+
+        static async Task dl_task()
+        {
+            string uriString = "";
+            string localDownloadPath = @"G:\data\azure";
+
+            Uri sasUri = new Uri(uriString);
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(sasUri);
+
+            foreach (BlobItem blob in blobContainerClient.GetBlobs())
+            {
+                string fileName = blob.Name;
+                BlobItemProperties prop = blob.Properties;
+                string localFilePath = Path.Combine(localDownloadPath, fileName);
+
+                if(download_track(localDownloadPath, fileName, prop.ContentHash)) 
+                {
+                    using (var file = File.Open(localFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                    {
+                        var blobClient = blobContainerClient.GetBlobClient(blob.Name);
+                        await blobClient.DownloadToAsync(file);
+
+                    }
+                }
+            }
+        }
+
+        static bool download_track(string dl_path, string filename, byte[] hash_code)
+        { 
+
+        }
+
+    }
+}

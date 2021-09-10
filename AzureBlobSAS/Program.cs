8fe0dc4ede8926b2d1abac86a01eb51a96e020b0
Lin <lin.special@gmail.com>
2021-09-09 21:15:31 -0400

    Merge pull request #1 from onedollor/add-license-1
    Create LICENSE

--- AzureBlobSAS/Program.cs
+++ AzureBlobSAS/Program.cs
@@ -11,40 +11,65 @@ namespace AzureBlobSAS
     {
         static void Main(string[] args)
         {
-            Task dl = dl_task();
+            string sasUristring = ConfigurationManager.AppSettings.Get("SasUri");
+            string localDownloadPath = @"G:\data\azure";
+
+            Task dl = DownloadContainerTask(localDownloadPath, sasUristring);
             dl.Wait();
         }
 
-        static async Task dl_task()
+        static async Task DownloadContainerTask(string localDownloadPath, string sasUristring)
         {
-            string uriString = ConfigurationManager.AppSettings.Get("SasUri");
-            string localDownloadPath = @"G:\data\azure";
-
-            Uri sasUri = new Uri(uriString);
+            Uri sasUri = new Uri(sasUristring);
 
             BlobContainerClient blobContainerClient = new BlobContainerClient(sasUri);
 
-            foreach (BlobItem blob in blobContainerClient.GetBlobs())
+            Azure.Pageable<BlobItem> blobs = blobContainerClient.GetBlobs();
+
+            foreach (BlobItem blob in blobs)
             {
                 string fileName = blob.Name;
                 BlobItemProperties prop = blob.Properties;
                 string localFilePath = Path.Combine(localDownloadPath, fileName);
 
-                if(download_track(localDownloadPath, fileName, prop.ContentHash)) 
+                if (fileName.IndexOf("/") >= 0) 
+                {
+                    string folder = fileName.Substring(0, fileName.LastIndexOf("/"));
+                    string localFolder = Path.Combine(localDownloadPath, folder);
+                    if (!Directory.Exists(localFolder)) 
+                    {
+                        Directory.CreateDirectory(localFolder);
+                    }
+                }
+
+                if(!FileExist(localFilePath, BitConverter.ToString(prop.ContentHash).Replace("-", "").ToLowerInvariant()))
                 {
                     using (var file = File.Open(localFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
                     {
                         var blobClient = blobContainerClient.GetBlobClient(blob.Name);
                         await blobClient.DownloadToAsync(file);
-
                     }
                 }
             }
         }
 
-        static bool download_track(string dl_path, string filename, byte[] hash_code)
+        static bool FileExist(string filename, string blobContentHash)
         {
-            return true;
+            string md5 = CalculateMD5(filename);
+
+            return md5.Equals(blobContentHash);
+        }
+
+        static string CalculateMD5(string filename)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
         }
 
     }
---
+++ AzureBlobSAS/PullBlob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobSAS
+{
+    public class PullBlob
+    {
+        public PullBlob(string SasUri, string localDownloadPath, string rowDelimiter, string qualifier, string encoding, int fieldCount) 
+        { 
+            
+        }
+    }
+}
